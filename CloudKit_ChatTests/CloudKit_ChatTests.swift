df1aa8128f27cc1b39deed1c65ad241f24c7d3be
Jarosław Pawlak <jaroslaw.pawlak@alan-systems.com>
2017-03-13 15:06:13 +0100

    Initial Commit

---
+++ CloudKit_ChatTests/CloudKit_ChatTests.swift
@@ -0,0 +1,36 @@
+//
+//  CloudKit_ChatTests.swift
+//  CloudKit_ChatTests
+//
+//  Created by Jarosław Pawlak on 13.03.2017.
+//  Copyright © 2017 Jarosław Pawlak. All rights reserved.
+//
+
+import XCTest
+@testable import CloudKit_Chat
+
+class CloudKit_ChatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
